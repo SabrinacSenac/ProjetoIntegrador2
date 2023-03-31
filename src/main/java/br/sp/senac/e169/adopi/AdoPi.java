22ab554fef66826de3d566e3e71cf001169e6228
Sabrinac Senac <sabrinacribas@gmail.com>
2023-03-31 12:18:40 -0300

    Primeiro Commit

---
+++ src/main/java/br/sp/senac/e169/adopi/AdoPi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.sp.senac.e169.adopi;
+
+/**
+ *
+ * @author SABRINA
+ */
+public class AdoPi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
